051d6e524a323003e3dfeefb194bf81ebb90b961
Johx22 <johannjose2004@gmail.com>
2022-04-16 17:09:46 +0300

    Adjust OF_STATUS_INDENT_LEFT

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -78,7 +78,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# Screen Settings
 	export OF_SCREEN_H=2400
 	export OF_STATUS_H=105
-	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_LEFT=140
 	export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
 	export OF_ALLOW_DISABLE_NAVBAR=0
