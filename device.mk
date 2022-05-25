5a98c0576e1bb408467abf74919b82c9ea000748
Johx22 <johannjose2004@gmail.com>
2022-05-25 14:20:20 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 	ashmemd_aidl_interface-cpp \
 	libashmemd_client
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+RECOVERY_LIBRARY_SOURCE_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
